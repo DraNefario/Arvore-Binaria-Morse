38ff733e70c7f3e953454fc434d884a5d082d6ff
Thomas Manussadjian Steinhausser <162912616+DraNefario@users.noreply.github.com>
2025-10-18 13:59:34 -0300

    implementaçao da classe arvoreBinariaMorse
    Adicionei os metodos de inserçao, busca, remoçao e exibiçao.

--- src/ArvoreBinariaMorse.java
+++ src/ArvoreBinariaMorse.java
@@ -1,3 +1,81 @@
 public class ArvoreBinariaMorse {
+    No raiz;
 
+    public ArvoreBinariaMorse() {
+        raiz = new No('\0');
+    }
+
+    public void inserir(String morse, char simbolo){
+        No atual = raiz;
+        for (int i = 0; i < morse.length(); i++) {
+            char s = morse.charAt(i);
+            if(s == '.'){
+                if(atual.getEsquerdo() == null){
+                    atual.setEsquerdo(new No('\0'));
+                }
+                atual = atual.getEsquerdo();
+            }else if(s == '-'){
+                if(atual.getDireito() == null){
+                    atual.setDireito(new No('\0'));
+                }
+                atual = atual.getDireito();
+            }
+        }
+        atual.setSimbolo(simbolo);
+    }
+
+    public char buscar(String morse){
+        No atual = raiz;
+        for (int i = 0; i < morse.length(); i++) {
+            char s = morse.charAt(i);
+            if(s == '.'){
+                if(atual.getEsquerdo() == null){
+                    return '\0';
+                }
+                atual = atual.getEsquerdo();
+            }else if(s == '-'){
+                if(atual.getDireito() == null){
+                    return '\0';
+                }
+                atual = atual.getDireito();
+            }
+        }
+        return atual.getSimbolo();
+    }
+
+    public void remover(String morse){
+        No atual = raiz;
+
+        for (int i = 0; i < morse.length(); i++) {
+            char s = morse.charAt(i);
+
+            if(s == '.'){
+                atual =  atual.getEsquerdo();
+            } else if (s == '-') {
+                atual = atual.getDireito();
+            }
+
+            if (atual == null){
+                return;
+            }
+        }
+        atual.setSimbolo('\0');
+    }
+
+    public void exibirMorse(String morse){
+        exibirRecursivo(raiz, "");
+    }
+
+    public void exibirRecursivo(No no, String caminho){
+        if(no == null){
+            return;
+        }
+
+        if(no.getSimbolo() != '\0'){
+            System.out.println("[" + caminho + "] " + no.getSimbolo());
+        }
+
+        exibirRecursivo(no.getEsquerdo(), caminho + ".");
+        exibirRecursivo(no.getDireito(), caminho + "-");
+    }
 }

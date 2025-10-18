70dd3822c1627ca542f6da6f794855293c6fd9c5
Thomas Manussadjian Steinhausser <162912616+DraNefario@users.noreply.github.com>
2025-10-18 14:16:44 -0300

    teste1
    criacao da nova arvore e inserçao de elementos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        ArvoreBinariaMorse arvore = new ArvoreBinariaMorse();
+
+        arvore.inserir(".-", 'A');
+        arvore.inserir("-...", 'B');
+        arvore.inserir("-.-.", 'C');
+        arvore.inserir("...", 'S');
+        arvore.inserir("---", 'O');
 
     }
 }

13d39469e4c5ae95689233b80f98abd3c2d430da
Thomas Manussadjian Steinhausser <162912616+DraNefario@users.noreply.github.com>
2025-10-18 14:21:20 -0300

    inserçao da tabela morse completa

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,40 @@ public class Main {
         arvore.inserir(".-", 'A');
         arvore.inserir("-...", 'B');
         arvore.inserir("-.-.", 'C');
-        arvore.inserir("...", 'S');
+        arvore.inserir("-..", 'D');
+        arvore.inserir(".", 'E');
+        arvore.inserir("..-.", 'F');
+        arvore.inserir("--.", 'G');
+        arvore.inserir("....", 'H');
+        arvore.inserir("..", 'I');
+        arvore.inserir(".---", 'J');
+        arvore.inserir("-.-", 'K');
+        arvore.inserir(".-..", 'L');
+        arvore.inserir("--", 'M');
+        arvore.inserir("-.", 'N');
         arvore.inserir("---", 'O');
+        arvore.inserir(".--.", 'P');
+        arvore.inserir("--.-", 'Q');
+        arvore.inserir(".-.", 'R');
+        arvore.inserir("...", 'S');
+        arvore.inserir("-", 'T');
+        arvore.inserir("..-", 'U');
+        arvore.inserir("...-", 'V');
+        arvore.inserir(".--", 'W');
+        arvore.inserir("-..-", 'X');
+        arvore.inserir("-.--", 'Y');
+        arvore.inserir("--..", 'Z');
+
+        arvore.inserir("-----", '0');
+        arvore.inserir(".----", '1');
+        arvore.inserir("..---", '2');
+        arvore.inserir("...--", '3');
+        arvore.inserir("....-", '4');
+        arvore.inserir(".....", '5');
+        arvore.inserir("-....", '6');
+        arvore.inserir("--...", '7');
+        arvore.inserir("---..", '8');
+        arvore.inserir("----.", '9');
 
 
         System.out.println("Estrutura da arvore:");

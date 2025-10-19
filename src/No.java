956580dae1d1b27917263456e5db6a16be747b4d
Guilherme006 <guilherme_lazari@outlook.com>
2025-10-18 23:07:28 -0300

    Refatora classe No

--- src/No.java
+++ src/No.java
@@ -1,35 +1,12 @@
 public class No {
-    private char simbolo;
-    private No esquerdo;
-    private No direito;
 
-    public No(char simbolo) {
-        this.simbolo = simbolo;
+    public String valor;
+    public No esquerdo;
+    public No direito;
+
+    public No() {
+        this.valor = "";
         this.esquerdo = null;
         this.direito = null;
-    }
-
-    public char getSimbolo() {
-        return simbolo;
-    }
-
-    public void setSimbolo(char simbolo) {
-        this.simbolo = simbolo;
-    }
-
-    public No getEsquerdo() {
-        return esquerdo;
-    }
-
-    public void setEsquerdo(No esquerdo) {
-        this.esquerdo = esquerdo;
-    }
-
-    public No getDireito() {
-        return direito;
-    }
-
-    public void setDireito(No direito) {
-        this.direito = direito;
     }
 }
